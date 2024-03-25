d3edc98bb53df4f3a542495dbcea5ba0e2d7e1da
reivaJSnevet <128876020+reivaJSnevet@users.noreply.github.com>
2024-03-24 21:45:48 -0600

    Add Dosc to useLocalStorage hook

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,5 +1,14 @@
 import { useState, useEffect} from "react";
 
+/**
+ * Retrieves a value from the local storage based on the provided key.
+ * If the value does not exist in the local storage, it returns the initial value.
+ * If the initial value is a function, it executes the function and returns its result.
+ *
+ * @param {string} key - The key used to retrieve the value from the local storage.
+ * @param {*} initValue - The initial value to be returned if the value does not exist in the local storage.
+ * @returns {*} The value retrieved from the local storage or the initial value.
+ */
 const getLocalValue = (key, initValue) => {
     if(typeof window === "undefined") {
         return initValue;
@@ -17,6 +26,13 @@ const getLocalValue = (key, initValue) => {
     return initValue;
 }
 
+/**
+ * Custom hook for managing state in local storage.
+ *
+ * @param {string} key - The key to use for storing the value in local storage.
+ * @param {*} initValue - The initial value to use if no value is found in local storage.
+ * @returns {Array} - An array containing the current value and a function to update the value.
+ */
 const useLocalStorage = (key, initValue) => {
     const [value, setValue] = useState(()=>{
         return getLocalValue(key, initValue);
