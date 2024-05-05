80ab765092962a7fec47857ec10ad67ae0d0d941
reivaJSnevet <128876020+reivaJSnevet@users.noreply.github.com>
2024-05-05 01:12:55 -0600

    Update ForgotPassword component UI and styles

--- src/pages/forgotPassword/ForgotPassword.jsx
+++ src/pages/forgotPassword/ForgotPassword.jsx
@@ -24,16 +24,13 @@ const ForgotPassword = () => {
 
   return (
     <div className="flex items-center justify-center h-screen bg-slate-50 ">
-      <div className="flex max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-2xl lg:max-w-4xl">
-        <img
-          className="hidden object-cover lg:block lg:w-1/2 brightness-50"
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Question_mark_%28black%29.svg/800px-Question_mark_%28black%29.svg.png"
-          alt="casa olvidada"
-        />
-        <div className="w-full p-10 lg:w-1/2">
-          <h2 className="mb-4 text-xl font-bold text-gray-800">
+      <div className="w-full max-w-md overflow-hidden bg-white rounded-lg shadow-lg md:w-2/3 lg:w-1/2">
+        <div className="px-4 py-2 bg-[#abcc5779]">
+          <h2 className="text-xl font-bold text-gray-800">
             ¿Has olvidado tu contraseña?
           </h2>
+        </div>
+        <div className="relative max-w-md px-4 py-3 borderrounded ">
           <span className="block sm:inline">
             Por favor, introduce tu <strong>correo electrónico</strong> para que
             podamos enviarte un enlace de recuperación.
@@ -46,7 +43,7 @@ const ForgotPassword = () => {
               Correo electrónico
             </label>
             <input
-              className="w-full px-3 py-2 mb-3 leading-tight text-gray-700 border rounded focus:outline-none focus:shadow-outline"
+              className="w-full px-3 py-2 mb-1 leading-tight text-gray-700 border rounded focus:outline-none focus:shadow-outline"
               id="email"
               type="email"
               placeholder="Ingrese su correo"
@@ -56,8 +53,15 @@ const ForgotPassword = () => {
                 setEmail(e.target.value);
               }}
             />
+            {msg && (
+              <div
+                className={`flex items-center justify-center text-center font-semibold text-sm ${msgTextColor}`}
+              >
+                {msg}
+              </div>
+            )}
             <div className="flex flex-col items-center justify-center mt-6 space-y-4 sm:flex-row sm:space-y-0 sm:space-x-4">
-              <button className="w-full px-4 py-2 text-sm font-semibold text-white bg-green-800 rounded hover:bg-green-900">
+              <button className="w-full px-4 py-2 text-sm font-semibold text-white bg-green-700 rounded hover:bg-green-900">
                 Buscar correo
               </button>
               <button
@@ -70,13 +74,6 @@ const ForgotPassword = () => {
               </button>
             </div>
           </form>
-          {msg && (
-            <div
-              className={`flex items-center justify-center mt-10 text-center font-semibold ${msgTextColor}`}
-            >
-              {msg}
-            </div>
-          )}
         </div>
       </div>
     </div>
@@ -84,3 +81,22 @@ const ForgotPassword = () => {
 };
 
 export default ForgotPassword;
+
+<div className="relative max-w-md px-4 py-3 borderrounded ">
+  <span className="block sm:inline">
+    Por favor, introduce tu <strong>correo electrónico</strong> para que podamos
+    enviarte un enlace de recuperación.
+  </span>
+  <div className="relative mt-6">
+    <label className="block mb-2 font-semibold text-gray-700" htmlFor="email">
+      Correo electrónico
+    </label>
+    <input
+      className="w-full px-3 py-2 mb-3 leading-tight text-gray-700 border rounded focus:outline-none focus:shadow-outline"
+      id="email"
+      type="email"
+      placeholder="Ingrese su correo electrónico"
+      autoComplete="off"
+    />
+  </div>
+</div>;
