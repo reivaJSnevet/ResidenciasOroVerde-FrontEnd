a06086071f89f86792f2da1f0b1b350ad1393204
reivaJSnevet <128876020+reivaJSnevet@users.noreply.github.com>
2024-05-28 18:27:07 -0600

    Remove unused code

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -3,7 +3,7 @@ import Layout from "../components/layout/Layout";
 import Home from "../pages/home/home1/Home.jsx";
 import Admin from "../pages/administrador/Administrador";
 import Users from "../pages/administrador/users/Users";
-import Roles from "../pages/administrador/roles/Roles"
+import Roles from "../pages/administrador/roles/Roles";
 import Categories from "../pages/administrador/categories/Categories";
 import Properties from "../pages/administrador/properties/Properties";
 import Login from "../pages/login/Login";
@@ -22,49 +22,46 @@ import NotFound from "../pages/notFound/NotFound.jsx";
 import AboutUs from "../pages/aboutUs/AboutUs.jsx";
 import Profile from "../pages/profile/Profile.jsx";
 
-
-
 const routes = (
-    <>
-      <Route element={<PersistLogin />}>
-        <Route path="/" element={<Home2 />} />
-        <Route path="login" element={<Login />} />
-        <Route path="register" element={<Register />} />
-        <Route path="forgot-password" element={<ForgotPassword />} />
-        
-        <Route path="/reset-password" element={<ResetPassword />}>
-            <Route path=":token" element={<ResetPassword />} />
-        </Route>
+  <>
+    <Route element={<PersistLogin />}>
+      <Route path="/" element={<Home2 />} />
+      <Route path="login" element={<Login />} />
+      <Route path="register" element={<Register />} />
+      <Route path="forgot-password" element={<ForgotPassword />} />
 
-        <Route path="/" element={<Layout />}>
-            <Route path="/aboutUs" element={<AboutUs />} />
-            <Route path="/mapa" element={<Home />} />
-            <Route path="/propiedades" element={<Propiedades />} />
-            <Route path="/forRent" element={<ForRent />} />
-            <Route path="/forSale" element={<ForSale />} />
-            <Route path="/propiedad" element={<Propiedad />}>
-                <Route path=":id" element={<Propiedad />} />
-               
-            </Route>
-            <Route path="/profile" element={<Profile />} />
-
-                <Route element={<RequireAuth allowedRoles={["admin"]} />}>
-
-                    <Route path="/admin" element={<Admin />}>
-                        <Route path="usuarios" element={<Users />} />
-                        <Route path="roles" element={<Roles />} />
-                        <Route path="categorias" element={<Categories />} />
-                        <Route path="propiedades" element={<Properties />} />
-                        <Route path="propiedad" element={<Propiedad />} />
-                    </Route>
-
-                </Route>
-            
-        </Route>
-        <Route path="unauthorized" element={<Unauthorized />} />
-        <Route path="*" element={<NotFound />} />
+      <Route path="/reset-password" element={<ResetPassword />}>
+        <Route path=":token" element={<ResetPassword />} />
       </Route>
-    </>
+
+      <Route path="/" element={<Layout />}>
+        <Route path="/aboutUs" element={<AboutUs />} />
+        <Route path="/mapa" element={<Home />} />
+        <Route path="/propiedades" element={<Propiedades />} />
+
+        <Route element={<RequireAuth allowedRoles={["admin"]} />}>
+          <Route path="/propiedad" element={<Propiedad />}>
+            <Route path=":id" element={<Propiedad />} />
+          </Route>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
+
+        <Route element={<RequireAuth allowedRoles={["admin"]} />}>
+          <Route path="/admin" element={<Admin />}>
+            <Route path="usuarios" element={<Users />} />
+            <Route path="roles" element={<Roles />} />
+            <Route path="categorias" element={<Categories />} />
+            <Route path="propiedades" element={<Properties />} />
+            <Route path="propiedad" element={<Propiedad />} />
+          </Route>
+        </Route>
+      </Route>
+
+      <Route path="unauthorized" element={<Unauthorized />} />
+      <Route path="*" element={<NotFound />} />
+
+    </Route>
+  </>
 );
 
 export default routes;
