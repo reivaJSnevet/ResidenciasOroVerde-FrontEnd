fba65eba9c716963b7d56d7f8c46903a9c7d5638
Rosi-07 <cuberoflores07@gmail.com>
2024-06-12 13:08:05 -0600

    added contactUs component

--- src/pages/contactUs/ContactUs.jsx
+++ src/pages/contactUs/ContactUs.jsx
@@ -1,9 +1,50 @@
 import { Email, WhatsApp } from '@mui/icons-material';
-import React from 'react';
+import { useState } from 'react';
+import api from '../../database/api';
+import { useSnackbar } from 'notistack';
 
-const Contact = () => {
+const ContactUs = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: ''
+  });
+  const { enqueueSnackbar } = useSnackbar();
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!formData.name || !formData.email || !formData.message) {
+      enqueueSnackbar('Todos los campos son obligatorios', { variant: 'error' });
+      return;
+    }
+
+    try {
+      const response = await api.post('/contact', formData);
+      if (response.status === 200) {
+        enqueueSnackbar('Correo enviado correctamente', { variant: 'success' });
+        setFormData({
+          name: '',
+          email: '',
+          message: ''
+        });
+      }
+    } catch (error) {
+      enqueueSnackbar('Error al enviar el correo', { variant: 'error' });
+    }
+  };
+
+
+
   return (
-    <section className="relative" >
+    <section className="relative">
       <div className="w-full h-[400px] bg-cover bg-center brightness-50"
         style={{ backgroundImage: 'url("https://www.slidebackground.com/uploads/real-estate-background/real-estate-homebuy-home-banner-background-homebuym-1.jpg")' }}>
       </div>
@@ -42,25 +83,52 @@ const Contact = () => {
                 </ul>
               </div>
               <div className="h-fit p-5 md:p-12">
-                <form>
+                <form onSubmit={handleSubmit}>
                   <div className="mb-6">
                     <div className="mx-0 mb-1 sm:mb-4">
                       <div className="mx-0 mb-1 sm:mb-4">
                         <label htmlFor="name"></label>
-                        <input type="text" id="name"  placeholder="Nombre" className="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md " name="name" />
+                        <input
+                          type="text"
+                          id="name"
+                          placeholder="Nombre"
+                          className="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md"
+                          name="name"
+                          value={formData.name}
+                          onChange={handleChange}
+                        />
                       </div>
-                      <div >
+                      <div>
                         <label htmlFor="email"></label>
-                        <input type="email" id="email" autoComplete="email" placeholder="Correo electrónico" className="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md " name="email" />
+                        <input
+                          type="email"
+                          id="email"
+                          autoComplete="email"
+                          placeholder="Correo electrónico"
+                          className="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md"
+                          name="email"
+                          value={formData.email}
+                          onChange={handleChange}
+                        />
                       </div>
                     </div>
                     <div>
                       <label htmlFor="textarea"></label>
-                      <textarea id="textarea" name="textarea" rows="5" placeholder="Comentario..." className="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md"></textarea>
+                      <textarea
+                        id="message"
+                        name="message"
+                        rows="5"
+                        placeholder="Mensaje..."
+                        className="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md"
+                        value={formData.message}
+                        onChange={handleChange}
+                      ></textarea>
                     </div>
                   </div>
                   <div className="text-center">
-                    <button type="submit" className="w-full bg-[#61dd67] text-white px-6 py-3 rounded-md ">Enviar mensaje</button>
+                    <button type="submit" className="w-full bg-[#61dd67] hover:bg-[#3c6c42] text-white px-6 py-3 rounded-md">
+                      Enviar mensaje
+                    </button>
                   </div>
                 </form>
               </div>
@@ -72,4 +140,4 @@ const Contact = () => {
   );
 };
 
-export default Contact;
+export default ContactUs;
