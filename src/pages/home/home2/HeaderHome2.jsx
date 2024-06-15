db91d967a655c129de208d4097712e1ad9a1c50b
Rosi-07 <cuberoflores07@gmail.com>
2024-06-14 22:16:23 -0600

    add login/profile in home2

--- src/pages/home/home2/HeaderHome2.jsx
+++ src/pages/home/home2/HeaderHome2.jsx
@@ -1,12 +1,20 @@
+import useAuthStore from "../../../hooks/auth/useAuth";
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import Navbar from "./Navbar";
 import Logo from "./logo";
 
+
 const HeaderHome2 = ({topSales = [], topRents = []}) => {
+  const auth = useAuthStore((state) => state.auth);
+  const location = useLocation();
   const [showRentCards, setShowRentCards] = useState(false);
   const [showSaleCards, setShowSaleCards] = useState(false);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  
+
+
 
   const navBgClass = showRentCards || showSaleCards ? "bg-white text-black" : "text-white";
 
@@ -23,8 +31,13 @@ const HeaderHome2 = ({topSales = [], topRents = []}) => {
 
       <nav className="flex justify-end flex-grow basis-0">
         <ul className="flex text-sm [&>li>a]:transition-colors [&>li>a]:duration-500 [&>li>a]:text-current [&>li>a]:font-medium [&>li>a]:inline-block [&>li>a]:px-4 [&>li>a]:py-2">
-          <li><Link to="/login">Iniciar sesión</Link></li>
-          <li><Link to="/register">Registrarse</Link></li>
+        {
+                        auth.accessToken ?
+                            <Link to="/profile" className="">Perfil</Link> :
+                            <Link to="/login" state={{ from: location.pathname }} 
+                            className="px-4 py-2 transition duration-500 border-2 border-transparent hover:bg-gray-200 hover:border-white hover:bg-black hover:bg-opacity-20"
+                            >Iniciar Sesión</Link>}
+          
         </ul>
       </nav>
 
