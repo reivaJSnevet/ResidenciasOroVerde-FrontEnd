793188187ed91aea50dd77737ce6a98ed5985aeb
Rosi-07 <cuberoflores07@gmail.com>
2024-06-15 00:04:25 -0600

    changes in header home

--- src/components/layout/headers/Header.jsx
+++ src/components/layout/headers/Header.jsx
@@ -1,56 +1,96 @@
+import { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import useAuthStore from '../../../hooks/auth/useAuth';
-import Logo from '../../../pages/home/home2/logo';
+import LogoG from '../../../pages/home/home1/LogoG';
+import { GridMenuIcon } from '@mui/x-data-grid';
+import { CloseOutlined } from '@mui/icons-material';
+
 
 function Header() {
     const auth = useAuthStore((state) => state.auth);
     const location = useLocation();
+    const [showMenu, setShowMenu] = useState(false);
 
     return (
-        <header id="header-home1" className="sticky z-40 flex items-center justify-between w-full px-10 text-black ">
+        <header id="header-home1" className="sticky top-0 z-40 flex items-center justify-between w-full px-4 bg-gray-180 bg-opacity-90 backdrop-blur-sm shadow-md transition-shadow duration-300 ease-in-out">
             <div className="flex flex-grow basis-0">
-                <a href="" className="w-auto">
-                    <Logo />
+                <a href="/" className="w-auto">
+                    <LogoG />
                 </a>
             </div>
 
-            <nav className="relative hidden xl:block sm:hidden">
-                <ul className="flex text-sm [&>li>a]:transition-colors [&>li>a]:duration-500 [&>li>a]:text-current [&>li>a]:font-medium [&>li>a]:inline-block [&>li>a]:px-4 [&>li>a]:py-2">
-                    <li><Link
-                     className="px-4 py-2 transition duration-500 border-2 border-transparent hover:bg-gray-200 hover:border-white hover:bg-black hover:bg-opacity-20"
-                      to="/">Inicio</Link></li>
 
-                    <li className="relative">
-                        <Link
-                         className="px-4 py-2 transition duration-500 border-2 border-transparent hover:bg-gray-200 hover:border-white hover:bg-black hover:bg-opacity-20" 
-                         to="/propiedades" >Búsqueda</Link>
-                        
-                        <Link 
-                         className="px-4 py-2 transition duration-500 border-2 border-transparent hover:bg-gray-200 hover:border-white hover:bg-black hover:bg-opacity-20"
-                        to="/contactUs">Contacto</Link>
-
-                        <Link 
-                         className="px-4 py-2 transition duration-500 border-2 border-transparent hover:bg-gray-200 hover:border-white hover:bg-black hover:bg-opacity-20"
-                        to="/aboutUs">Sobre nosotros</Link>
-                    </li>
+            <nav className="hidden xl:flex text-sm">
+                <ul className="flex space-x-4">
+                    <NavLink to="/">Inicio</NavLink>
+                    <NavLink to="/propiedades">Búsqueda</NavLink>
+                    <NavLink to="/contactUs">Contacto</NavLink>
+                    <NavLink to="/aboutUs">Sobre nosotros</NavLink>
                 </ul>
+            </nav>
+
+         
+            <nav className="flex xl:hidden items-center">
+                <button
+                    onClick={() => setShowMenu(!showMenu)}
+                    className="text-sm px-4 py-2 border border-transparent rounded-md text-gray-800 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-300"
+                >
+                    {showMenu ? <CloseOutlined /> : <GridMenuIcon />} 
+                </button>
+                {showMenu && (
+                    <div className="absolute right-0 mt-2 w-48 bg-white border border-gray-200 rounded-md shadow-lg z-10">
+                        <ul className="py-1">
+                            <MobileNavLink to="/" onClick={() => setShowMenu(false)}>Inicio</MobileNavLink>
+                            <MobileNavLink to="/propiedades" onClick={() => setShowMenu(false)}>Búsqueda</MobileNavLink>
+                            <MobileNavLink to="/contactUs" onClick={() => setShowMenu(false)}>Contacto</MobileNavLink>
+                            <MobileNavLink to="/aboutUs" onClick={() => setShowMenu(false)}>Sobre nosotros</MobileNavLink>
+                        </ul>
+                    </div>
+                )}
             </nav>
 
             <nav className="flex justify-end flex-grow basis-0">
-                <ul className="flex text-sm [&>li>a]:transition-colors [&>li>a]:duration-500 [&>li>a]:text-current [&>li>a]:font-medium [&>li>a]:inline-block [&>li>a]:px-4 [&>li>a]:py-2">
-                    {
-                        auth.accessToken ?
-                            <Link to="/profile" className="">Perfil</Link> :
-                            <Link to="/login" state={{ from: location.pathname }} 
-                            className="px-4 py-2 transition duration-500 border-2 border-transparent hover:bg-gray-200 hover:border-white hover:bg-black hover:bg-opacity-20"
-                            >Iniciar Sesión</Link>}
+                <ul className="flex text-sm">
+                    {auth.accessToken ? (
+                        <li>
+                            <Link to="/profile" className="px-4 py-2 transition duration-500 border-b-2 border-transparent hover:border-green-800">Perfil</Link>
+                        </li>
+                    ) : (
+                        <li>
+                            <Link to="/login" state={{ from: location.pathname }} className="px-4 py-2 transition duration-500 border-b-2 border-transparent hover:border-green-800">Iniciar Sesión</Link>
+                        </li>
+                    )}
                 </ul>
             </nav>
         </header>
-   
+    );
+}
+
+function NavLink({ to, children }) {
+    return (
+        <li>
+            <Link
+                to={to}
+                className="px-4 py-2 transition duration-500 border-b-2 border-transparent hover:border-green-800"
+            >
+                {children}
+            </Link>
+        </li>
+    );
+}
+
+function MobileNavLink({ to, children, onClick }) {
+    return (
+        <li>
+            <Link
+                to={to}
+                onClick={onClick}
+                className="block px-4 py-2 text-sm text-gray-800 hover:bg-gray-100 hover:text-gray-900"
+            >
+                {children}
+            </Link>
+        </li>
     );
 }
 
 export default Header;
-
-
---
+++ src/pages/home/home1/LogoG.jsx
@@ -0,0 +1,10 @@
+const LogoG = () => {
+    const logoPath = '/logoverde.png';
+    
+      return (
+        <img src={logoPath} alt="Company Logo"  className="w-auto h-20 text-black"  />
+      );
+    };
+    
+    export default LogoG;
+    
